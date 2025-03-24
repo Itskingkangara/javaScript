ce33471a02bc62c8c123337a4998264ec53083a1
Harshit Gupta <collegevidha@gmail.com>
2025-03-24 11:49:09 +0000

    some Conversions

--- 01_basics/02_basics.js
+++ 01_basics/02_basics.js
@@ -1,9 +1,27 @@
 let num= 10
-let string=" Yeap Its ME "
+let string=" 100"
 let bool =true;
 let num2=null //means num abhi kuch nhi h 
                 //no value is assigned with num2 
 
-console.log(typeof true)
-console.log(typeof null) //it reutrns null is a object
-console.log(typeof undefined) //output will undefined
+// console.log(typeof true)
+// console.log(typeof null) //it reutrns null is a object
+// console.log(typeof undefined) //output will undefined
+
+let changeIntoNum=Number(undefined)
+
+console.log(undefined)
+
+console.log(changeIntoNum);
+
+let tempNum=0;
+let converter=Boolean(tempNum);
+
+console.log(converter)
+
+
+//now we will try to convert in into string 
+let tempNumber =100;
+let tempString =String(tempNumber);
+
+console.log(typeof (tempString));
