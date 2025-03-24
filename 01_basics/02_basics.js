e33f35590b5789077b59e2e83a22ba6278ea7854
Harshit Gupta <collegevidha@gmail.com>
2025-03-24 11:25:59 +0000

    file added

---
+++ 01_basics/02_basics.js
@@ -0,0 +1,9 @@
+let num= 10
+let string=" Yeap Its ME "
+let bool =true;
+let num2=null //means num abhi kuch nhi h 
+                //no value is assigned with num2 
+
+console.log(typeof true)
+console.log(typeof null) //it reutrns null is a object
+console.log(typeof undefined) //output will undefined
