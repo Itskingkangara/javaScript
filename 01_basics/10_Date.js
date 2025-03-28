583e5e1ad06c322757145cc9e6f0606e7189f86a
Harshit Gupta <collegevidha@gmail.com>
2025-03-28 08:41:55 +0000

    01 basics last file

---
+++ 01_basics/10_Date.js
@@ -0,0 +1,14 @@
+// const date= new Date()
+
+// console.log(date.toString())
+// console.log(typeof date)
+
+// const newDate= new Date(2023,20,22,12, 30 , 40 , 100)
+
+// console.log(newDate.toDateString())
+// console.log(newDate.toTimeString())
+
+//new type of formatting
+const newDateFormate= new Date("12-31-2002")
+
+console.log(newDateFormate.toDateString())
