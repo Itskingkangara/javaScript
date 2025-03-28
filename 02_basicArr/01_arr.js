1750a0c43d659ffd54931e98e3cb051a77f19d02
Harshit Gupta <collegevidha@gmail.com>
2025-03-28 11:24:14 +0000

    arr basics complete

---
+++ 02_basicArr/01_arr.js
@@ -0,0 +1,25 @@
+const arr=[0,1,"Harsh", true] //in arr diff data type can be used 
+
+//shallow copy 
+//change in elment will change the arr for eg if element no 3 get changed than arr's element will also change
+arr[3]=0
+
+console.log(arr[3])
+
+
+//another way of declaration
+let newArray = new Array(1,2,3,4,5,6,7,8,9)
+
+// console.log(newArray[4])
+newArray.unshift(9) //add element in start
+console.log(newArray)
+
+newArray.shift() //removes 1st element of arr 
+
+//although they are not considered as good options becz the hole arr need to be shift 
+console.log(newArray)
+let joinFunction = newArray.join();
+
+console.log(joinFunction) //converts function into string 
+
+ 
---
+++ 02_basicArr/02_arr.js
@@ -0,0 +1,40 @@
+const arr1=[1,2,3,4]
+const arr2=['a','b','c','d']
+
+// arr1.push(arr2)
+
+// console.log(arr1) //as array supports multiple data type it accepted whole array as a input
+
+const mergeArr = arr1.concat(arr2)
+
+// console.log(mergeArr) //HELP to merge 2 arr
+
+//if we want to merge more than 2 arr than it work like this
+
+// console.log(...arr1, ...arr2, ...mergeArr)
+
+let nestedArr= [1,2,3,['a','b','c',['ab','bc','ca']],5]
+
+let unNested=nestedArr.flat(2) //flat use when arr of arrays given 
+// console.log(unNested)
+
+// *******************************************
+
+//different functions of arr
+//when Input is not arr and you want to check and convert it into array
+console.log(Array.isArray("Harshit"))
+const arr=[1,2,3,4,5,6]
+console.log(Array.isArray(arr))
+
+const name="Harshit"
+console.log(Array.from(name)) //converts any data type into Array and if not possible then return empty Array
+
+//exceptional case
+console.log(Array.from({name: "Harshit"})) //it returns empty array
+
+console.log(Array.of(1,2,3,4,5,6,'a','b'))
+
+
+
+
+
