89984286971839ffca314ad769f59c150000e82b
Harshit Gupta <collegevidha@gmail.com>
2025-08-11 07:34:20 +0000

    revision of 01_basics

--- 01_basics/08_newKeyword.js
+++ 01_basics/08_newKeyword.js
@@ -1,7 +1,9 @@
-let string= new Number(100)
+let string= new Number("100") 
+
+// console.log(typeof string)
 
 //this Number keyword ensures that the keyword will be Number
 
 string =string.toString()
-console.log(string)
-console.log(typeof( string )); 
+// console.log(string)
+console.log(typeof( string ))
